0decd4195007ea244272e045e9668fe318f00977
CarlaSS1 <sisoncarla579@gmail.com>
2019-04-04 09:46:26 -0400

    Added XML documentation for Problem 1 class.

--- Assignment5/StuffedAnimal.cs
+++ Assignment5/StuffedAnimal.cs
@@ -20,14 +20,34 @@ namespace Assignment5
         // TO DO - Make 3 properties that describes the toy and have
         // 2 method which are actions that the toy can perform.
 
+       
+        /// <summary>
+        /// Initializes a new instance of <see cref="StuffedAnimal"/> class.
+        /// </summary>
         public StuffedAnimal()
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="StuffedAnimal"/> class.
+        /// </summary>
+        /// <param name="id">Global Unique ID.</param>
         public StuffedAnimal(Guid id) : base(id)
         {
         }
 
+        /// <summary>
+        /// Initalizes a new instance of <see cref="StuffedAnimal"/> class.
+        /// </summary>
+        /// <param name="id">Global Unique ID.</param>
+        /// <param name="cost">The cost.</param>
+        /// <param name="description">The description.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="manufacturingCompany">The manufacturing company.</param>
+        /// <param name="year">The year.</param>
+        /// <param name="ageLimitMax">Maximum age limit.</param>
+        /// <param name="ageLimitMin">Minimum age limit.</param>
+        /// <param name="weight">The weight</param>
         public StuffedAnimal(Guid id, double cost, string description, string name, string manufacturingCompany, int year, int ageLimitMax, int ageLimitMin, double weight) : base(id, cost, description, name, manufacturingCompany, year, ageLimitMax, ageLimitMin, weight)
         {
         }
--- Assignment5/Toy.cs
+++ Assignment5/Toy.cs
@@ -15,33 +15,88 @@ namespace Assignment5
     /// </summary>
     public abstract class Toy
     {
+        /// <summary>
+        /// Gets or sets a global unique ID
+        /// </summary>
+        /// <value>Global Unique ID.</value>
         public Guid Id { get; set; }
 
+        /// <summary>
+        /// Gets or sets the cost.
+        /// </summary>
+        /// <value>The cost.</value>
         public double Cost { get; set; }
 
+        /// <summary>
+        /// Gets or sets Description
+        /// </summary>
+        /// <value>The description</value>
         public string Description { get; set; }
 
+        /// <summary>
+        /// Gets or sets Name
+        /// </summary>
+        /// <value>The name.</value>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Gets or sets the Manufacturing company
+        /// </summary>
+        /// <value>The manufacturing company.</value>
         public string ManufacturingCompany { get; set; }
 
+        /// <summary>
+        /// Gets or sets a year.
+        /// </summary>
+        /// <value>The year.</value>
         public int Year { get; set; }
 
+        /// <summary>
+        /// Gets or sets the maximun age limit.
+        /// </summary>
+        /// <value>The maximum age limit.</value>
         public int AgeLimitMax { get; set; }
 
+        /// <summary>
+        /// Gets or sets the minimum age limit.
+        /// </summary>
+        /// <value>The minimum age limit</value>
         public int AgeLimitMin { get; set; }
 
         // Choking Hazard - Enum List or a Enum?
 
+        /// <summary>
+        /// Gets or sets the weight.
+        /// </summary>
+        /// <value>The weight.</value>
         public double Weight { get; set; }
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="Toy"/> class.
+        /// </summary>
+        /// <param name="id">Global Unique ID.</param>
         protected Toy(Guid id)
         {
             Id = id;
         }
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="Toy"/> class.
+        /// </summary>
         public Toy() {}
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="Toy"/> class.
+        /// </summary>
+        /// <param name="id">Global Unique ID.</param>
+        /// <param name="cost">The cost.</param>
+        /// <param name="description">The description.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="manufacturingCompany">The manufacturing company.</param>
+        /// <param name="year">The year.</param>
+        /// <param name="ageLimitMax">Maximum age limit.</param>
+        /// <param name="ageLimitMin">Minimum age limit.</param>
+        /// <param name="weight">The weight</param>
         public Toy(Guid id, double cost, string description, string name, string manufacturingCompany, int year, int ageLimitMax, int ageLimitMin, double weight) : this(id)
         {
             Cost = cost;
